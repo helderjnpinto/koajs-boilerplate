eacff38041703542fbd1d83722a81f58952dab0e
Helder <helder@yarilabs.com>
2018-04-10 11:28:12 +0100

    resolv conflits

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 var koa = require('koa');
 const mount = require('koa-mount');
-var koa = require('koa')
-var app = new koa()
+var koa = require('koa');
+var app = new koa();
 // const koaBody = require('koa-body');
 var bodyParser = require('koa-bodyparser');
 
-import routes from './routes'
+import routes from './routes';
 
 var app = new koa();
 app.use(bodyParser());
@@ -13,4 +13,4 @@ app.use(routes);
 // app.use(koaBody({
 //     jsonLimit: '1kb'
 // }));
-export default app
+export default app;
