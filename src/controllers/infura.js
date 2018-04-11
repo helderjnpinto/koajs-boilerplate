08bad950400be305e53102af91a5bc3cb44f7e14
Helder <helder@yarilabs.com>
2018-04-11 20:10:48 +0100

    test some web3 calls

--- src/controllers/infura.js
+++ src/controllers/infura.js
@@ -1,30 +1,95 @@
-import {
-  web3api as web3
+import web3i, {
+  web3
 } from '../infura';
 
 
-
-
-const _batchRequest = function(account, batch) {
-  if (!batch) {
-    var batch = web3.web3Instance.createBatch();
-    batch.add(web3.eth.getBalance.request(account, 'latest', (res) => {console.log(res) }) );
-    batch.execute();
-  } else {
-    // 
-  }
+/**
+ * _batchRequest 
+ * - Batch process is not working
+ * @param {string} account 
+ * @param {web3i} _web3i 
+ */
+const _batchRequest = function (account, _web3i) {
+  var batch = new _web3i.web3.BatchRequest();
+  batch.add(_web3i.eth.getBalance.request(account, 'latest', (res) => {
+    console.log('res', res);
+  }));
+  batch.execute();
+  console.log('res batch', batch);
 }
+
 
 export const get = async (ctx, next) => {
-  // console.log('web3 deb: ', web3.eth)
-  let res;
   const q = ctx.query;
 
-  _batchRequest(web3.account);
+  const account = web3i.personalAccount;
+  _setDefaultAccount(web3i, account);
 
 
-  ctx.body = res;
-  await next();
+  const balance = await _getBalance(web3i, account);
+  const trans = await _getTransaction(web3i, '0xf3954b44881adc77a05bc2ab1538032344e8e2542ad68e3d391eaedcb7c08859');
+  ctx.body = {
+    balance: _convertWeiToEth(web3i, balance),
+    trans
+    // gasPrice: await _getGasPrice(web3i)
+  }
+  await next()
 }
 
 
+
+/**
+ * _getBalance
+ *  
+ * @param {any} account 
+ * @param {any} _web3i 
+ * @param {string} [block='latest'] 
+ * @returns Promise with data
+ */
+const _getBalance = async function (_web3i, account, block = 'latest') {
+  return _web3i.eth.getBalance(account, block);
+}
+
+/**
+ * set default account eth account 
+ * 
+ * @param {any} _web3i 
+ * @param {any} addr 
+ * @see http://web3js.readthedocs.io/en/1.0/web3-eth.html#defaultaccount
+ */
+const _setDefaultAccount = (_web3i, addr) => {
+  _web3i.eth.defaultAccount = addr;
+}
+
+/**
+ * Convert to wei to eth
+ * 
+ * @param {any} _web3i 
+ * @param {any} wei 
+ * @returns 
+ */
+const _convertWeiToEth = (_web3i, wei) => {
+  return _web3i.web3.utils.fromWei(wei, 'ether');
+}
+
+/**
+ * Get current gas price in eth
+ * 
+ * @param {any} _web3i 
+ * @returns 
+ */
+const _getGasPrice = async (_web3i) => {
+  return _convertWeiToEth(_web3i, await _web3i.eth.getGasPrice());
+}
+
+
+/**
+ * Get transaction details
+ * 
+ * @param {any} _web3i 
+ * @param {any} txHash 
+ * @returns 
+ */
+const _getTransaction = async (_web3i, txHash ) => {
+  return await _web3i.eth.getTransaction(txHash);
+}
--- src/infura/index.js
+++ src/infura/index.js
@@ -1,11 +1,23 @@
+/**
+ * @module Infura provider
+ */
 import Web3 from 'web3';
 
 const infrink = 'https://rinkeby.infura.io/FdJHU8ZABTe7w1V3hFkb';
-const web3 = new Web3(infrink || 'http://localhost:8545');
-const personalAccountTestRink = '0x8717eD44cEB53f15dB9CF1bEc75a037A70232AC8';
+/**
+ * The HTTP provider is deprecated, as it won’t work for subscriptions.
+ */
+const providerHttps = new Web3.providers.HttpProvider(infrink)
+export const web3 = new Web3(providerHttps);
 
-export const web3api = {
-        web3Instance: web3,
+const personalAccountTestRink = '0x8717eD44cEB53f15dB9CF1bEc75a037A70232AC8';
+/**
+ *  Internal instance for web3
+ */
+const web3i = {
+        web3,
         eth: web3.eth,
-        personalAccount: personalAccountTestRink
+        personalAccount: personalAccountTestRink,
+        httpsProvider: providerHttps
 }
+export default web3i;
