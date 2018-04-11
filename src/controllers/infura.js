14c03b056fa13b483092790c2a39a40dce515e1b
Helder <helder@yarilabs.com>
2018-04-11 16:13:32 +0100

    wip

--- src/controllers/infura.js
+++ src/controllers/infura.js
@@ -1,10 +1,30 @@
-import { web3api as web3 } from '../infura';
+import {
+  web3api as web3
+} from '../infura';
 
-export const get = async (ctx, next) => {
-  console.log('web3 deb: ', web3.eth)
-  ctx.body = JSON.stringify(web3.account);
-  await next()
+
+
+
+const _batchRequest = function(account, batch) {
+  if (!batch) {
+    var batch = web3.web3Instance.createBatch();
+    batch.add(web3.eth.getBalance.request(account, 'latest', (res) => {console.log(res) }) );
+    batch.execute();
+  } else {
+    // 
+  }
 }
 
+export const get = async (ctx, next) => {
+  // console.log('web3 deb: ', web3.eth)
+  let res;
+  const q = ctx.query;
+
+  _batchRequest(web3.account);
+
+
+  ctx.body = res;
+  await next();
+}
 
 
--- src/infura/index.js
+++ src/infura/index.js
@@ -1,11 +1,11 @@
 import Web3 from 'web3';
 
 const infrink = 'https://rinkeby.infura.io/FdJHU8ZABTe7w1V3hFkb';
-const personalAccountTestRink = 'https://rinkeby.infura.io/FdJHU8ZABTe7w1V3hFkb';
-const web3 = new Web3(infrink || 'http:localhost');
+const web3 = new Web3(infrink || 'http://localhost:8545');
+const personalAccountTestRink = '0x8717eD44cEB53f15dB9CF1bEc75a037A70232AC8';
 
 export const web3api = {
         web3Instance: web3,
         eth: web3.eth,
-        account: personalAccountTestRink
+        personalAccount: personalAccountTestRink
 }
