5afa3754d4fb69db661b42f8486800784ced665b
Helder <helder@yarilabs.com>
2018-04-11 15:25:58 +0100

    add strong params

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ import routes from './routes';
 
 var koa = require('koa');
 var bodyParser = require('koa-bodyparser');
+var params = require('koa-strong-params');
+
 var app = new koa();
 
 app.use(routes);
 app.use(bodyParser());
+app.use(params());
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
+/** @module Server*/
 import app from './app'
 var logger = require('koa-logger')
 
 
 app.use(logger())
-console.log('...', process.env.NODE_ENV)
+//TODO: configure envs console.log('...', process.env.NODE_ENV)
 
 app.listen(4000)
 console.log('listening on port 4000')
